bcb8884846a8fb9f7f60ba7e33f835840597d2fb
Stuart Kam <stuart.kam17@gmail.com>
2016-06-21 17:19:26 -1000

    first cut of charges controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
+  #support stripe through charges
+
+  resources :charges
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
