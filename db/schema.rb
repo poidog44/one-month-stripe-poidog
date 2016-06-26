bad2393715b5231dc4df6b3eb3e7be1f18300849
Stuart Kam <stuart.kam17@gmail.com>
2016-06-25 21:07:41 -1000

    Implemented subscriptiong based product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626060211) do
+ActiveRecord::Schema.define(version: 20160626065554) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20160626060211) do
     t.string   "author_image_name"
     t.text     "author_description"
     t.string   "download_url"
+    t.string   "details"
   end
 
   create_table "purchases", force: :cascade do |t|
